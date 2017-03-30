ce2cba659bef3dd9f31808578d9b031b3efe9413
Gino Messmer <g.messmer@ftsoftware.eu>
2017-03-30 16:44:33 +0200

    Simple setup and Unit Test project

---
+++ UnitTestFakesLiftoff.Core.Test/SimpleTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFakesLiftoff.Core.Test
+{
+    [TestClass]
+    public class SimpleTest
+    {
+        [TestMethod]
+        public void SimpleTestMethod()
+        {
+        }
+    }
+}
---
+++ UnitTestFakesLiftoff.Core/Common/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestFakesLiftoff.Core.Common
+{
+    class ViewModelBase
+    {
+    }
+}
