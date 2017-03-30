2982c9c822377ae4089ebf0507b514dd68b0de3f
Gino Messmer <g.messmer@ftsoftware.eu>
2017-03-30 16:50:23 +0200

    Simple app service

---
+++ UnitTestFakesLiftoff.Core/Service/AppService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestFakesLiftoff.Core.Service
+{
+    public class AppService : IAppService
+    {
+        public string DisplayCurrentTimeOfDay()
+        {
+            return DateTime.Now.TimeOfDay.ToString();
+        }
+
+        public string GetCurrentWeather()
+        {
+            // Do it with some network stuff
+            return "Sonny";
+        }
+    }
+}
